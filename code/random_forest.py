e23b16a76d5ab75e9b28553674f804e2f3ff3f75
gott51010 <yomiyomi0724@hotmail.com>
2020-01-28 13:05:25 +0900

    随机森林

--- code/random_forest.py
+++ code/random_forest.py
@@ -1,9 +1,11 @@
 """随机森林"""
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_wine
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
-import pandas as pd
-import pydotplus
+
 import graphviz
 
 
@@ -11,57 +13,36 @@ import graphviz
 # 分类树组成的森林就叫随机森林分类器
 # 回归树....................回归器
 
+# 单个的决策树准确率越高 森林的准确率也会越高
+
+# 随机森林里的参数
+# n_estimators 森林里基数评估器的数量(即 树的数量)
+# 当n_estimators达到一定程度后 森林的精准度也开始波动
+# 这个参数越大 需要的计算量和内存也越大 训练时间越长
+
+# 开始愉快地建个森林吧!
 wine = load_wine()
-a = pd.concat([pd.DataFrame(wine.data), pd.DataFrame(wine.target)], axis=1)
-# 表的最后一列是target
-# print(a)
-
-# wine 里还有几个属性是接下来要用的
-# 特证名 feature_names
-# 分类名 target_names
-
-# 分训练集和测试集
+#  分训练集和测试集
 Xtrain, Xtest, Ytrain, Ytest = train_test_split(wine.data, wine.target, test_size=0.3)
-# test_size参数是指0.3作为测试集 0.7作为训练集
-# print(wine.data.shape)   # 原始数据是(178, 13) 178行13个标签
-# print(Xtrain.shape)  # 训练集是(124, 13) 124行13个标签
 
+# 生成一棵树
+clf = DecisionTreeClassifier(random_state=0)
+# 生成森林
+rfc = RandomForestClassifier(random_state=0)
+# 注意! random_state=0 指定 也不能保证每次结果相同
+# 因为DecisionTreeClassifier random_state=0 是生成同一棵树
+# RandomForestClassifier random_state=0 是生成同一片森林 里面的每棵树是长得不一样的
 
-# 开始愉快地建模吧!
-# 实例化
-# clf = tree.DecisionTreeClassifier(criterion="entropy")
-# random_state 参数设置分支随机性
-# splitter 参数设置随即选项 "best"优先选择更重要的特征分类  "random"分支更随机,树更深,训练集拟合度降低
-clf = tree.DecisionTreeClassifier(criterion="entropy")
 clf = clf.fit(Xtrain, Ytrain)
-score = clf.score(Xtest, Ytest)  # 注意参数顺序
-print(score)  # 预测精确度  我这次跑是0.9629629629629
+rfc = rfc.fit(Xtrain, Ytrain)
+
+score_c = clf.score(Xtest, Ytest)
+score_r = rfc.score(Xtest, Ytest)
+
+# print(score_c)    # 本次0.8888888888888888
+# print(score_r)    # 本次0.9629629629629629
+# 每次运行结果不一样,但是多跑几次差不多可以看出 森林的准确度一般大于等于单棵决策树
 
 
-# 更直观的数据 决策树可视化
-# 里面放入刚建好的模型
-dot_data = tree.export_graphviz(clf, feature_names=wine.feature_names,
-                                class_names=["beer", "juice", "water"],
-                                filled=True,    # 分类颜色 以及颜色越浅当前分类不纯度越高
-                                rounded=True    # 圆角方形
-                                )
-
-# 跑不了这个库的话 需要安装Graphviz
-graph = pydotplus.graph_from_dot_data(dot_data)
-# 利用pydotplus 把决策树导出为图片(pdf也可)
-with open('../LogisticTest/tree_graph.png', 'wb') as f:
-    f.write(graph.create_png())
-
-# 特征重要性 feature_importances_
-# print(clf.feature_importances_)
-# 可见特征里只有4项是有数字的 说明对于构造这个决策树是只有这四项是有重要意义的
-"""
-[0.10151147 0.         0.         0.         0.         0.
- 0.41155238 0.         0.         0.18719674 0.         0.
- 0.29973942]
-"""
-# zip 关联特征和重要性
-show_zip = {*zip(wine.feature_names, clf.feature_importances_)}
-print(show_zip)
 
 
